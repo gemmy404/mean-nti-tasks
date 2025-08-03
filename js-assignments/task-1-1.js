c3541da8e339ab5f8bb7107219b1719abc4a65ae
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2025-08-03 21:28:44 +0300

    add task 1 from first session

---
+++ js-assignments/task-1-1.js
@@ -0,0 +1,31 @@
+console.log("============Task 1============");
+// 1.
+var id = 223937;
+var theName = "Mahmoud Gamal";
+var isAttended = true;
+var typeNull = null, typeUndefined = undefined;
+
+// 2.
+console.log("Id:", id);
+console.log("Name:", theName);
+console.log("Is attended:", isAttended);
+console.log("Type null:", typeNull);
+console.log("Type undefined:", typeUndefined);
+
+console.log("#".repeat(30));
+
+// 3.
+console.log("Type of id:", typeof id);
+console.log("Type of name:", typeof theName);
+console.log("Type of isAttended:", typeof isAttended);
+console.log("Type of null:", typeof typeNull);
+console.log("Type of Undefined:", typeof typeUndefined);
+
+console.log("#".repeat(30));
+
+// 4.
+var firstName = "Mahmoud", lastName = "Gamal";
+console.log("Fullname with old way:", firstName + " " + lastName);
+console.log("Fullname with backtick way:", `${firstName} ${lastName}`);
+
+console.log("#".repeat(30));
